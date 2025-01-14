510c8f23521f553f1f10f1447c566e6f377026b2
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2025-01-13 20:23:00 -0500

    update dependencies

--- common-ui/build.gradle.kts
+++ common-ui/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     addAndroidDefaultLib()
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
